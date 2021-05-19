07754c63aaa726a51e77ab4f3e36b5bf734c0ad6
Matheus-AM <mam.macacod@gmail.com>
2021-05-18 23:21:01 -0300

    added some divisions

---
+++ solucoes/questao1/cliente.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/wait.h>
+
+#define PORT 53
+#define BUFFMAX 256    //tamanho maximo da string
+
+int main (int argc, char * * argv) {
+
+    struct sockaddr_in network;
+
+    int sock;
+
+    char buffer_in[BUFFMAX];
+    char buffer_out[BUFFMAX];
+    int request, response, length;
+
+    if (argc < 2) {
+        printf ("Use %s <host>\n\n", argv [0]);
+        exit (0);
+    }
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    //domain –  AF_INET for IPv4/ AF_INET6 for IPv6 
+    //type – SOCK_STREAM for TCP / SOCK_DGRAM for UDP 
+    //0 means use default protocol for the address family.
+
+    if (sock == -1) {
+        perror("Socket");
+        exit(0);
+    }
+
+    bzero ((char *)&network, sizeof (network));
+    network.sin_family = AF_INET;
+    network.sin_port = htons(PORTA);
+    network.sin_addr.s_addr = htonl(INADDR_ANY); 
+
+    bind(sock, (struct sockaddr*) &network, sizeof(network));
+
+    printf("IP: %s\n", INADDR_ANY);
+
+    while(1){
+
+        printf ("\nMensagem: ");
+
+        scanf("%s", &buffer_out);
+
+        request = sendto(sock, buffer_out, BUFFMAX, 0, (struct sockaddr *) &network, (socklen_t) sizeof(network));
+
+        if (request == -1) {
+            perror("Sendto");
+            exit (0);
+        }
+
+        response = recvfrom(sock, buffer_in, BUFFMAX, 0, (struct sockaddr *) &network, (socklen_t*)&length);
+
+        if (response == -1) {
+            perror("Recvfrom");
+            exit (0);
+        }
+
+        if(!strcmp(buffer_in, "exit")){
+            break;
+        }
+
+        printf ("\nResposta: %s", buffer_in);
+    }
+
+    return 0;
+}
